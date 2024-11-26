a82b535db07491484f72c89f44f0ad409c1734b2
omkar1103 <115880514+omkar1103@users.noreply.github.com>
2024-11-26 11:41:01 +0530

    Files added

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-$.scrollIt();
+document.addEventListener("scroll", function () {
+    const header = document.querySelector(".main-header");
+    if (window.scrollY > 50) { // Adjust the scroll position as needed
+        header.classList.add("fixed-header");
+    } else {
+        header.classList.remove("fixed-header");
+    }
+});

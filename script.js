4dedfc6b5580d7310ec9ed8b2496f0ee8c0dd297
omkar1103 <115880514+omkar1103@users.noreply.github.com>
2024-12-27 11:54:00 +0530

    made changes in projet section

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener("scroll", function () {
     const header = document.querySelector(".main-header");
-    if (window.scrollY > 50) { // Adjust the scroll position as needed
+    if (window.scrollY > 50) {
         header.classList.add("fixed-header");
-    } else {
+    } else {3
         header.classList.remove("fixed-header");
     }
 });
+
+
